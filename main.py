63d09c427517055df2b51bb43cb03997ae7ad746
Kola Ayonrinde <koayon@gmail.com>
2024-03-21 15:43:35 +0000

    Scaffold StatefulModel

--- main.py
+++ main.py
@@ -1,33 +1,97 @@
+from typing import Optional
+
 import torch as t
-from transformers import AutoTokenizer, MambaConfig, MambaForCausalLM
+from transformers import (
+    AutoTokenizer,
+    MambaConfig,
+    MambaForCausalLM,
+    PretrainedConfig,
+    PreTrainedModel,
+)
 from transformers.models.mamba.modeling_mamba import MambaCache, MambaCausalLMOutput
 
+
+class StatefulModel(PreTrainedModel):
+    def __init__(
+        self,
+        model: MambaForCausalLM,
+        initial_state: Optional[MambaCache],
+        device: Optional[str] = None,
+    ):
+        super().__init__(model.config)
+        if initial_state is None:
+            initial_state = MambaCache(config=model.config, batch_size=1, device=device)
+        self.initial_state = initial_state
+        self.model = model
+
+    def forward(
+        self,
+        input_ids: t.Tensor,
+        cache_params: Optional[MambaCache] = None,
+        reset_sequence_offset: bool = True,
+    ) -> MambaCausalLMOutput:
+        # TODO: Deal with change in batch size
+        if cache_params is None:
+            cache_params = self.initial_state
+        if reset_sequence_offset:
+            cache_params = self.reset_state_offset(cache_params)
+
+        out: MambaCausalLMOutput = self.model(
+            input_ids=input_ids, cache_params=cache_params, use_cache=True
+        )
+        return out
+
+    def build_state(
+        self,
+        input_ids: t.Tensor,
+        save_state: bool,
+        cache_params: Optional[MambaCache] = None,
+    ) -> MambaCache:
+        out: MambaCausalLMOutput = self.forward(input_ids=input_ids, cache_params=cache_params)
+        assert out.cache_params is not None
+        if save_state:
+            self.save_state(out.cache_params)
+        return out.cache_params
+
+    def save_state(self, state: MambaCache) -> None:
+        raise NotImplementedError
+
+    def load_state(self) -> None:
+        raise NotImplementedError
+
+    def combine_states(self, state1: MambaCache, state2: MambaCache) -> MambaCache:
+        raise NotImplementedError
+
+    def update_state(self, state: MambaCache) -> None:
+        self.initial_state = state
+
+    def reset_state_offset(self, state: MambaCache) -> MambaCache:
+        state.seqlen_offset = 0
+        return state
+
+
 if __name__ == "__main__":
-    model = MambaForCausalLM(MambaConfig())
+    # Set up
+    stateless_model = MambaForCausalLM(MambaConfig())
     tokeniser = AutoTokenizer.from_pretrained("state-spaces/mamba-130m-hf")
     input_ids: t.Tensor = tokeniser("Hey how are you doing?", return_tensors="pt")[  # type: ignore
         "input_ids"
     ]
 
-    out: MambaCausalLMOutput = model(input_ids=input_ids, use_cache=True)
+    # State generation with no initial state
+    model = StatefulModel(
+        model=stateless_model,
+        initial_state=None,
+    )
+    generated_state = model.build_state(input_ids=input_ids, save_state=False)
+    model.update_state(generated_state)
+
+    # Reuse of state with forward, generating a new state
+    out: MambaCausalLMOutput = model(input_ids=input_ids)
     assert out.cache_params is not None
     assert out.logits is not None
     saved_cache_params = out.cache_params
-    saved_cache_params.seqlen_offset = 0
-    # We probably need a reset_sequence_offset: bool attribute in our function (defaulting to True)
 
     print(saved_cache_params.ssm_states[1].shape)
     print("ssm_state", saved_cache_params.ssm_states[0])
-    print("output", out.logits[0, -1, :])
-
-    # Rerun the model with the saved cache
-    out: MambaCausalLMOutput = model(
-        input_ids=input_ids, use_cache=True, cache_params=saved_cache_params
-    )
-    assert out.cache_params is not None
-    assert out.logits is not None
-
-    new_cache_params = out.cache_params
-    print(new_cache_params.ssm_states[1].shape)
-    print("ssm_state", new_cache_params.ssm_states[0])
     print("output", out.logits[0, -1, :])

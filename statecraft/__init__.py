2cb8fc3d77cf1156a4062067af7ab04f68a92853
Kola Ayonrinde <koayon@gmail.com>
2024-03-25 01:06:20 +0000

    Rename main to core

--- statecraft/__init__.py
+++ statecraft/__init__.py
@@ -1,3 +1,3 @@
 "Store, manage and remix states for SSMs and other Stateful models"
 
-from statecraft.main import SSMStateMetadata, StatefulModel
+from statecraft.core import SSMStateMetadata, StatefulModel

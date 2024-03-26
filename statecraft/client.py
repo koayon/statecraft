ce77d438d4392d4385b6e9f13d92274434594aab
Kola Ayonrinde <koayon@gmail.com>
2024-03-26 16:10:22 +0000

    Add get_states API call to client

--- statecraft/client.py
+++ statecraft/client.py
@@ -6,6 +6,7 @@ from typing import Union
 import requests
 
 from statecraft.metadata import SSMStateMetadata
+from statecraft.models import StateOut, StatesOut
 from statecraft.user_attributes import UserAttributes
 from statecraft.utils import get_default_cache_dir
 
@@ -52,6 +53,7 @@ class StatecraftClient:
             state_files = {"state_file": f}
 
             response = requests.post(cls.states_url, params=query_params, files=state_files)
+            # TODO: Type this output
         return response.json()
 
     @classmethod
@@ -71,11 +73,35 @@ class StatecraftClient:
         else:
             raise ValueError("User attributes not found. Please run statecraft.setup()")
 
+    @classmethod
+    def get_states(cls, model_name: str) -> list[str]:
+        model_user_name, model_short_name = model_name.split("/")
+        query_params = {
+            "model_user_name": model_user_name,
+            "model_short_name": model_short_name,
+        }
+
+        response = requests.get(
+            cls.states_url,
+            params=query_params,
+        )
+        parsed_response: list[dict[str, str]] = response.json()  # StatesOut
+
+        states_list = [
+            f'{response_state_dict["model_name"]}/{response_state_dict["state_name"]}'
+            for response_state_dict in parsed_response
+        ]
+
+        return states_list
+
 
 if __name__ == "__main__":
     client = StatecraftClient()
     state = client.get_state("state-spaces/mamba-130m-hf", "test_user/test_a")
     print(state)
+
+    states = client.get_states("state-spaces/mamba-130m-hf")
+    print(states)
 
     # response_json = client.upload_state(
     #     SSMStateMetadata(

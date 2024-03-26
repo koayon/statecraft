294271fa953007ada2eb920b24cc211955940738
Kola Ayonrinde <koayon@gmail.com>
2024-03-26 13:49:24 +0000

    Scaffold tests for stateful_model

---
+++ tests/test_stateful_model.py
@@ -0,0 +1,14 @@
+# UNIT TESTS
+# Test generating state
+# RAG generate (expected failure)
+# Combine states
+# Update state
+# Forward with generated state and show logits are different
+
+
+# POSSIBLE INTEGRATION TESTS
+# Test loading model from pretrained (Hugging Face)
+# Test loading state from local cache
+# Test loading state from test_db
+# Test saving state to local cache
+# Test uploading state to test_db

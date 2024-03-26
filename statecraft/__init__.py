82ef2bcdc9c6570b65094b728b38d236358fc9a0
Kola Ayonrinde <koayon@gmail.com>
2024-03-26 13:48:21 +0000

    Add upload_state and list_local_states to top level imports

--- statecraft/__init__.py
+++ statecraft/__init__.py
@@ -1,3 +1,4 @@
 "Store, manage and remix states for SSMs and other Stateful models"
 
-from statecraft.core import SSMStateMetadata, StatefulModel
+from statecraft.core import SSMStateMetadata, StatefulModel, upload_state
+from statecraft.local_states import list_local_states

3647a43e4c95eae94bcfdd5b9a36a12316008f04
Kola Ayonrinde <koayon@gmail.com>
2024-04-03 12:27:56 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="statecraft",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(exclude=["tests", "docs"]),
     long_description=long_description,
     long_description_content_type="text/markdown",

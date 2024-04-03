493a42b526aa4b92de1ab0c119f56c5381463328
Kola Ayonrinde <koayon@gmail.com>
2024-04-03 23:38:00 +0100

    Refactor cache_to_device into within MambaCache class method

--- statecraft/utils.py
+++ statecraft/utils.py
@@ -1,9 +1,5 @@
-import copy
 import getpass
 import os
-
-import torch as t
-from transformers.models.mamba.modeling_mamba import MambaCache
 
 
 def get_default_cache_dir():
@@ -14,22 +10,3 @@ def get_default_cache_dir():
         return os.path.expanduser(rf"C:\Users\{username}\.cache\statecraft")
     else:
         raise ValueError(f"Unsupported operating system: {os.name}")
-
-
-def cache_to_device(cache: MambaCache, device: str) -> MambaCache:
-    mamba_cache = copy.deepcopy(cache)
-
-    ssm_states = {
-        layer_num: layer_cache.to(device)
-        for layer_num, layer_cache in mamba_cache.ssm_states.items()
-    }
-
-    conv_states = {
-        layer_num: layer_cache.to(device)
-        for layer_num, layer_cache in mamba_cache.conv_states.items()
-    }
-
-    mamba_cache.ssm_states = ssm_states
-    mamba_cache.conv_states = conv_states
-
-    return mamba_cache

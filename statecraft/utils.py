d85faf0130091c0e790de56f375ed666e62351b0
Kola Ayonrinde <koayon@gmail.com>
2024-04-07 23:55:00 +0100

    Give alternative path for windows machines with no username (e.g. test rack)

--- statecraft/utils.py
+++ statecraft/utils.py
@@ -8,8 +8,11 @@ def get_default_cache_dir():
     if os.name == "posix":  # For Linux and macOS
         return os.path.expanduser("~/.cache/statecraft/")
     elif os.name == "nt":  # For Windows
-        username = getpass.getuser()
-        return os.path.expanduser(rf"C:\Users\{username}\.cache\statecraft")
+        try:
+            username = getpass.getuser()
+            return os.path.expanduser(rf"C:\Users\{username}\.cache\statecraft")
+        except:  # Test windows machine has no username so return current directory
+            return os.path.expanduser(rf".cache\statecraft")
     else:
         raise ValueError(f"Unsupported operating system: {os.name}")
 

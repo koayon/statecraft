d0c63311cf27637569ce3617474ea321e89e5e87
Kola Ayonrinde <koayon@gmail.com>
2024-04-07 17:25:02 +0100

    Add generation comparison test

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -13,7 +13,19 @@ def tensor_input_ids():
     return tokeniser(prompt, return_tensors="pt")["input_ids"]
 
 
-def test_generate(tensor_input_ids):
-    model = StatefulModel.from_pretrained("state-spaces/mamba-130m-hf", device="cpu")
-    out: t.LongTensor = model.generate(tensor_input_ids, max_length=10, num_return_sequences=1)  # type: ignore
+@pytest.mark.parametrize("state", ["koayon/rhyme-state", "koayon/state-a"])
+def test_generate(tensor_input_ids, state):
+    model = StatefulModel.from_pretrained("state-spaces/mamba-130m-hf", state, device="cpu")
+
+    print("Generation 1")
+    out: t.LongTensor = model.generate(tensor_input_ids, max_length=10, num_return_sequences=1, do_sample=False)  # type: ignore
     assert out.shape == (1, 10)
+
+    # Test that the model outputs change given the state even when greedy decoding
+
+    stateless_model = StatefulModel.from_pretrained("state-spaces/mamba-130m-hf", device="cpu")
+
+    print("Generation 2")
+    stateless_out: t.LongTensor = stateless_model.generate(tensor_input_ids, max_length=10, num_return_sequences=1, do_sample=False)  # type: ignore
+
+    assert not t.equal(stateless_out, out)

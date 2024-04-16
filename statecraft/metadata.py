ada97d3c13a3eee18ed24ec697d4ec850dad211e
Kola Ayonrinde <koayon@gmail.com>
2024-04-16 13:27:04 +0100

    Add docstring for metadata dataclass

--- statecraft/metadata.py
+++ statecraft/metadata.py
@@ -4,6 +4,26 @@ from typing import Optional
 
 @dataclass
 class SSMStateMetadata:
+    """
+    The metadata object for a cached state in a Stateful Model.
+    This is created when a state is built and is used for understanding the state's context and how to reproduce it.
+    The metadata is json serialised when a state is saved to disk.
+
+    Args
+    ----------
+    state_name : str
+        The short name of the state, used for the path and for uploads.
+    prompt : str
+        The prompt that was used to generate the state. Can also be a prompt_reference, a url which points to a prompt.
+    model_name : str
+        The name of the model that the state was created for. This is a Hugging Face model name.
+    description : str, optional
+        A description of the state that can be used to understand the context of the state or when to use it.
+    keywords : Optional[list[str]], optional
+        Tags to filter the state by on the Statecraft Hub, by default None
+
+    """
+
     state_name: str
     prompt: str
     model_name: str
@@ -18,7 +38,9 @@ class SSMStateMetadata:
 
     def __repr__(self) -> str:
         truncated_prompt = self._truncate(self.prompt, 50)
-        truncated_description = self._truncate(self.description, 50) if self.description else None
+        truncated_description = (
+            self._truncate(self.description, 50) if self.description else None
+        )
         return f"""SSMStateMetadata(state_name={self.state_name}, model_name={self.model_name},
     prompt={truncated_prompt},
     description={truncated_description},
